e0b78bf7f93a3b3f4ab2d1def4804e46ea834461
时嘉庆 <1011649121@qq.com>
2022-05-29 02:51:27 +0800

    1.7 开发了一个hello测试接口  地址: 	http://127.0.0.1:8880/hello

---
+++ src/main/java/com/shi/candlelight/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.shi.candlelight.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController//一般用来返回字符串controller用饭返回一个页面 因为时前后段分离说一一般都用restcondleller
+public class TestController {
+    @RequestMapping("/hello")//配置接口对于的请求地址 常用的四种请求可以配置value method请求方式
+    public String hello(){
+        return "candlelight";
+    }
+}
