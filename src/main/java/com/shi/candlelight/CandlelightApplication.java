3af1027883e1d73a13ad699b30a0f9b366b59902
时嘉庆 <1011649121@qq.com>
2022-05-29 01:47:49 +0800

    into、

---
+++ src/main/java/com/shi/candlelight/CandlelightApplication.java
@@ -0,0 +1,13 @@
+package com.shi.candlelight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandlelightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandlelightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shi/candlelight/CandlelightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shi.candlelight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandlelightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
