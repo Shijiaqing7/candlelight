f6a88af4fb72f24c06b3cd11a5e4acd9ee33a20b
时嘉庆 <1011649121@qq.com>
2022-05-29 22:10:54 +0800

    1.6添加slf4j启动提示 增加banner启动动画时嘉庆java

--- src/main/java/com/shi/candlelight/CandlelightApplication.java
+++ src/main/java/com/shi/candlelight/CandlelightApplication.java
@@ -1,13 +1,22 @@
 package com.shi.candlelight;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.core.env.Environment;
+@Slf4j
 @SpringBootApplication
 public class CandlelightApplication {
+	private static final Logger LOG = LoggerFactory.getLogger(CandlelightApplication.class);
+
 
 	public static void main(String[] args) {
-		SpringApplication.run(CandlelightApplication.class, args);
+		SpringApplication app = new SpringApplication(CandlelightApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }
