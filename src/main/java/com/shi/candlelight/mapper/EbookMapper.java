c9383e298b3f7ca0413de14a512bf59a7032c3e2
时嘉庆 <1011649121@qq.com>
2022-05-30 23:29:22 +0800

    2.5 新增ebook表 使用generator生成了持久层代码

---
+++ src/main/java/com/shi/candlelight/mapper/EbookMapper.java
@@ -0,0 +1,30 @@
+package com.shi.candlelight.mapper;
+
+import com.shi.candlelight.pojo.Ebook;
+import com.shi.candlelight.pojo.EbookExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface EbookMapper {
+    long countByExample(EbookExample example);
+
+    int deleteByExample(EbookExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Ebook record);
+
+    int insertSelective(Ebook record);
+
+    List<Ebook> selectByExample(EbookExample example);
+
+    Ebook selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByExample(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByPrimaryKeySelective(Ebook record);
+
+    int updateByPrimaryKey(Ebook record);
+}
