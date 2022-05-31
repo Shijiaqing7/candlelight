c86f228d4ab4c82b38a713b31e4acf62a7224bd6
时嘉庆 <1011649121@qq.com>
2022-05-31 21:52:26 +0800

    3.4 制作了the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    candlelight by 时嘉庆 1011649121@qq.com00
+  </a-layout-footer>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer',
+
+
+});
+</script>
+
