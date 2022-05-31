a57749b042d4a38ad58ac16593b14a693145b22c
时嘉庆 <1011649121@qq.com>
2022-05-31 11:52:30 +0800

    2.7 封装请求参数和返回参数

--- src/main/java/com/shi/candlelight/controller/EbookController.java
+++ src/main/java/com/shi/candlelight/controller/EbookController.java
@@ -1,7 +1,9 @@
 package com.shi.candlelight.controller;
 
 import com.shi.candlelight.pojo.Ebook;
+import com.shi.candlelight.req.EbookReq;
 import com.shi.candlelight.resp.CommonResp;
+import com.shi.candlelight.resp.EbookResp;
 import com.shi.candlelight.service.EbookService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +17,10 @@ public class EbookController {
     private EbookService ebookService;
 
     @RequestMapping("/ebook/list")//配置接口对于的请求地址 常用的四种请求可以配置value method请求方式
-    public CommonResp list() {
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
+    public CommonResp list(EbookReq req) {
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
         //将返回指封装到返回指结果类中返回 方便前端校验
-        List<Ebook> list = ebookService.list();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);//返回泛型数据，自定义类型
         return resp;
     }
--- src/main/java/com/shi/candlelight/service/EbookService.java
+++ src/main/java/com/shi/candlelight/service/EbookService.java
@@ -2,9 +2,14 @@ package com.shi.candlelight.service;
 
 import com.shi.candlelight.mapper.EbookMapper;
 import com.shi.candlelight.pojo.Ebook;
+import com.shi.candlelight.pojo.EbookExample;
+import com.shi.candlelight.req.EbookReq;
+import com.shi.candlelight.resp.EbookResp;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 //一般将逻辑处理写在service
@@ -12,7 +17,21 @@ import java.util.List;
 public class EbookService {
     @Resource//将ebookmapper注入进来也可以用jdk自带的resource
     private EbookMapper ebookMapper;
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req){
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%"+req.getName()+"%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            //ebookResp.setId(ebook.getId());这种比较麻烦 将ebok的指赋到返回值结果类中ebbokresp
+            //可以用beanutils中的copyporperties方法来完成复制
+            BeanUtils.copyProperties(ebook,ebookResp);//这样就实现了对象的复制
+            respList.add(ebookResp);
+
+        }
+        return respList;
+        }
     }
-}
+
